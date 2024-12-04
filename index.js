275a937725559779d1a4218301319d5865de507c
Kelvin Muchiri <muchirikelv71@gmail.com>
2024-12-04 12:45:27 +0300

    first changes

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // the array with strings 
 const stringArray = ["1", "2", "3"];
+
+//create a function which will convert it
+
+function convertArray(strings) {
+    return parseInt(strings);
+}
+const numArray = stringArray.map(convertArray); 
+
+
+
